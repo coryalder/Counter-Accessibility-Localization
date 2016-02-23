737bf772c6f381a8333697b383f7f174722771c4
Cory Alder <coryalder@gmail.com>
2016-02-23 10:57:39 -0800

    Added localization, settings, and copy/paste

--- Counter/AppDelegate.swift
+++ Counter/AppDelegate.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+typealias UIColour = UIColor
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -16,6 +18,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let 😙 = UIColour.blackColor()
+        "donut draught draft lite"
+        
+        
         return true
     }
 
--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -26,8 +26,59 @@ class ViewController: UIViewController {
     }
 
     func updateUI() {
-        countLabel.text = "The count is \(count)"
+        
+        let localized = NSLocalizedString("the count is _", comment: "showing the current count")
+        countLabel.text = String(format: localized, count)
+        //countLabel.text = "The count is \(count)"
+    }
+
+    
+    @IBAction func viewWasTapped(sender: AnyObject) {
+        
+        let menuController = UIMenuController()
+        
+        menuController.setTargetRect(self.countLabel.frame, inView: self.view)
+        
+        menuController.setMenuVisible(true, animated: true)
+        
     }
     
+    
+    override func canBecomeFirstResponder() -> Bool {
+        return true
+    }
+    
+    override func canPerformAction(action: Selector, withSender sender: AnyObject?) -> Bool {
+        
+        print("asked about \(action)")
+        
+        if action == "copy:" || action == "paste:" {
+            return true
+        }
+        return false
+    }
+    
+    override func copy(sender: AnyObject?) {
+        
+        let pb = UIPasteboard.generalPasteboard()
+//        pb.string = self.countLabel.text
+        pb.string = "\(count)"
+        print("copied \(pb.string)")
+    }
+    
+    override func paste(sender: AnyObject?) {
+        
+        let pb = UIPasteboard.generalPasteboard()
+        
+        if let pbString = pb.string,
+            let intValue = Int(pbString) {
+                count = intValue
+                updateUI()
+        } else {
+        
+        }
+//        self.countLabel.text = pb.string
+        
+    }
 }
 
