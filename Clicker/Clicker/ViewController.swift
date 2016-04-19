c2e87acebe2a61f699711456e15b5a02e5e3ce67
Cory Alder <coryalder@gmail.com>
2016-04-19 11:57:10 -0700

    After lecture

--- Clicker/Clicker/ViewController.swift
+++ Clicker/Clicker/ViewController.swift
@@ -17,11 +17,65 @@ class ViewController: UIViewController {
     @IBAction func increaseClickCount(sender: UIButton) {
         count += 1
         updateUI()
+        
+        let systemTitle1 = UIFont.preferredFontForTextStyle(UIFontTextStyleTitle1)
+        
+        let myFont = UIFont(name: "Marker Felt", size: systemTitle1.pointSize)
+        
+//        myLabel.font = myFont
     }
     
     func updateUI() {
-        self.clickCountLabel.text = "\(count) Clicks"
+        self.clickCountLabel.text = String(format: NSLocalizedString("_ clicks", comment: "the number of clicks currently displayed"), count)
     }
 
+    override func canBecomeFirstResponder() -> Bool {
+        return true
+    }
+    
+    override func canPerformAction(action: Selector, withSender sender: AnyObject?) -> Bool {
+        print("asked about \(action)")
+        
+        switch action {
+        case #selector(ViewController.copy(_:)),
+             #selector(ViewController.paste(_:)):
+            return true
+        default:
+            return false
+        }
+    }
+    
+    
+    override func copy(sender: AnyObject?) {
+        
+        let pb = UIPasteboard.generalPasteboard()
+        pb.string = "\(count)"
+//        pb.setData(<#T##data: NSData##NSData#>, forPasteboardType: <#T##String#>)
+    }
+    
+    
+    override func paste(sender: AnyObject?) {
+        
+        let pb = UIPasteboard.generalPasteboard()
+        
+        if let pasteString = pb.string,
+            let pastedNumber = Int(pasteString) {
+            count = pastedNumber
+        }
+        
+        updateUI()
+        
+    }
+    
+    
+    @IBAction func labelTapped(sender: UITapGestureRecognizer) {
+        let menuController = UIMenuController()
+        
+        menuController.setTargetRect(sender.view!.frame, inView: view)
+        
+        menuController.setMenuVisible(true, animated: true)
+        
+        print("tapped label")
+    }
 }
 
